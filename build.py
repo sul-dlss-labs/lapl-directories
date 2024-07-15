39ec489557249b63d77ba10fb235c8863fbd08d3
Ed Summers <ehs@pobox.com>
2024-07-15 08:43:05 -0400

    just use pathlib

--- build.py
+++ build.py
@@ -6,31 +6,34 @@ each page PDF to a single title PDF. The aggregated PDF is then written to the
 data directory using the document ID.
 """
 
-import glob
-import pathlib
+from pathlib import Path
 
 import pypdf
 
-def main():
 
+def main():
     for doc_dir in document_dirs():
         print(f"generating pdf for {doc_dir}")
 
         pdf = pypdf.PdfWriter()
         for pdf_file in sorted(doc_dir.iterdir()):
             print(f"adding {pdf_file}")
-            with pdf_file.open('rb') as fh:
+            with pdf_file.open("rb") as fh:
                 pdf.append(fh, pages=(0, 1))
 
         # turn the document directory into the document id by stripping the data
         # directory and converting the '/' to '_'
-        output_path = pathlib.Path('data') / (str(doc_dir).replace('data/', '').replace('/', '_') + '.pdf')
-        pdf.write(output_path.open('wb'))
+        output_path = Path("data") / (
+            str(doc_dir).replace("data/", "").replace("/", "_") + ".pdf"
+        )
+        pdf.write(output_path.open("wb"))
         pdf.close()
 
         print(f"wrote {output_path}")
 
+
 def document_dirs():
-    return [pathlib.Path(d) for d in glob.glob('data/*/*/*/*')]
+    return [Path(d) for d in Path('data').glob("*/*/*/*")]
+
 
 main()
